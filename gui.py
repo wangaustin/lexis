b7bfe954ad2de32cbb1999bd6c03b19080030965
Austin Wang <62589566+wangaustin@users.noreply.github.com>
2022-10-29 17:42:06 -0500

    not a pretty gui but it's in working condition

--- gui.py
+++ gui.py
@@ -31,9 +31,16 @@ def delete_temp_folder(localization_dir, temp_folder):
         os.chdir("..")
         os.rmdir(temp_folder)
 
+def clear_output():
+    textbox.configure(state="normal")
+    textbox.delete(1.0, "end")
+    textbox.configure(state="disabled")
+
 def find_all_by_key(prompt):
     # ask user which key they want to check the translations for
-    lbl_result["text"] = ""
+    textbox.configure(state="normal")
+    textbox.configure(wrap="none")
+    textbox.delete(1.0, "end")
 
     # start timing execution
     start_time = time.time()
@@ -96,7 +103,6 @@ def find_all_by_key(prompt):
 
     # -----------------------------------------------------------------------
     # find all translations for the key the user inputted
-    print("\n" + "---------------------------------")
 
     # get all the json files for all the languages
     all_json_files = os.listdir(os.getcwd())
@@ -128,11 +134,12 @@ def find_all_by_key(prompt):
         if not found:
             curr_output = "*** NO TRANSLATION! ***"
         
-        lbl_result["text"]+=(os.path.splitext(file)[0] + ":" + "\n" + "\t" + curr_output + "\n")
+        # lbl_result["text"]+=(os.path.splitext(file)[0] + ":" + "\n" + "\t" + curr_output + "\n")
+        textbox.insert("end", (os.path.splitext(file)[0] + ":" + "\n" + "\t" + curr_output + "\n"))
                 
         curr_file.close()
-
-    print("---------------------------------" + "\n")
+    
+    textbox.configure(state="disabled")
 
     # -----------------------------------------------------------------------
     # delete temp folder
@@ -258,37 +265,82 @@ def show_translations():
     # lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
     lbl_result["text"] = ent_lockey.get()
 
+
+
 # Set up the window
 window = tk.Tk()
 window.title("LEXIS GUI")
 window.minsize(1000, 600)
+
+# create a menu bar
+menu_bar = tk.Menu(window)
+window.config(menu=menu_bar)
+
+# add a menu
+file_menu = tk.Menu(menu_bar, tearoff=False)
+
+file_menu.add_command(
+    label="Exit",
+    command=window.destroy
+)
+
+menu_bar.add_cascade(
+    label="File",
+    menu=file_menu
+)
+
+
+info = tk.Label(master=window, text="Welcome to LEXIS!\nAuthor: Austin Wang (awwang@igs)\nWebsite: www.austinwang.co", anchor="w", justify="left")
+info["text"] += "\n----------------------------"
+info.grid(row=0, column=0)
 # window.resizable(width=False, height=False)
 
 # Create the Fahrenheit entry frame with an Entry
 # widget and label in it
 frm_prompt = tk.Frame(master=window)
 ent_lockey = tk.Entry(master=frm_prompt, width=30)
-lbl_temp = tk.Label(master=frm_prompt, text="Enter LOCTEXT key: ")
+lbl_temp = tk.Label(master=frm_prompt, text="Enter LOCTEXT key: ", font=(12))
 
 # Layout the temperature Entry and Label in frm_prompt
 # using the .grid() geometry manager
-ent_lockey.grid(row=0, column=1, sticky="w")
-lbl_temp.grid(row=0, column=0, sticky="w")
+lbl_temp.grid(row=1, column=0, sticky="w")
+ent_lockey.grid(row=1, column=1, sticky="w")
 
 # Create the conversion Button and result display Label
-btn_convert = tk.Button(
-    master=window,
+btn_find_by_key = tk.Button(
+    master=frm_prompt,
     text="Find Translations",
-    command=lambda: find_all_by_key(ent_lockey.get())
+    command=lambda: find_all_by_key(ent_lockey.get()),
+    anchor="w",
+    justify="left"
 )
-lbl_result = tk.Label(master=window, text="Translations will be shown here\n", anchor="w", justify="left")
-lbl_stat_array = []
+lbl_result = tk.Label(master=window, text="----------------------------\n", anchor="w", justify="left")
+
+# Create the conversion Button and result display Label
+btn_clear_output = tk.Button(
+    master=window,
+    text="Clear Output",
+    command=clear_output,
+    anchor="w",
+    justify="left"
+)
 
 # Set up the layout using the .grid() geometry manager
 
-frm_prompt.grid(row=0, column=0, padx=2)
-btn_convert.grid(row=0, column=1, pady=2)
-lbl_result.grid(row=1, column=0, pady=2, sticky="w")
+textbox = tk.Text(master=window, height=1, borderwidth=0)
+
+frm_prompt.grid(row=1, column=0, pady=2, sticky="w")
+btn_find_by_key.grid(row=2, column=0, pady=2, sticky="w")
+btn_clear_output.grid(row=2, column=0, pady=2, sticky="w")
+lbl_result["font"] = (14)
+lbl_result["state"] = "normal"
+lbl_result.grid(row=3, column=0, pady=2, sticky="w")
+
+textbox.grid(row=4, column=0, sticky="w")
+textbox["width"] = 140
+textbox["height"] = 30
+textbox.configure(state="normal")
+textbox.configure(inactiveselectbackground=textbox.cget("selectbackground"))
 
 # Run the application
 window.mainloop()
